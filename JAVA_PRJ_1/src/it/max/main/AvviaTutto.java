78e34048df188722833d47a316d14dc231cf45f3
maxquaro <m.quaroni1982@gmail.com>
2019-08-21 15:08:07 +0200

    primo commit

---
+++ JAVA_PRJ_1/src/it/max/main/AvviaTutto.java
@@ -0,0 +1,20 @@
+package it.max.main;
+/**
+ * 
+ * classe di avvio
+ * 
+ * @author max
+ * @version 1.0
+ * @
+*/
+public class AvviaTutto {
+  /**
+   * 
+   * @param args
+   */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
